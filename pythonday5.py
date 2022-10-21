f5b4b2f8c5d7bcae7e03f84c96c728c820b94d14
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-21 15:16:40 +0530

    Add files via upload

---
+++ pythonday5.py
@@ -0,0 +1,107 @@
+from re import A
+
+
+x = ("csptisn steve rogrers")
+y = ("thor odinson")
+z = ("tony atark")
+print(x, y, z)
+print(x.replace("s", "a"))
+print(x.replace("a", "r"))
+print(y.replace("o", "v"))
+print(y.replace("n", "o"))
+print(z.replace("o", "p"))
+print(z.replace("i", "u"))
+print(x.upper())
+print(x.lower())
+print(type(x))
+# do something that is variety
+x = "life is full of race it wont stop for us we have to stop it for us"
+y = "for achievig that we have to do it by own our selves beacuse we have to do this otherwise we will loose our hope  for our survival we have to do this"
+z = 87, 800, 635, 65, 34, 13
+print(x+y)
+print(y)
+print(z)
+print(x.upper())
+print(x.lower())
+print(x.replace("is", "apl"))
+print(x.replace("h", "ok"))
+print(x.replace("s", "hn"))
+print(x.replace("i", "yh"))
+print(x.replace("k", "ai"))
+print(x.replace("l", "po"))
+print(y.upper())
+print(y.lower())
+print(y.replace("is", "hl"))
+print(y.replace("h", "ok"))
+print(y.replace("s", "hn"))
+print(y.replace("i", "yh"))
+print(y.replace("k", "uj"))
+print(y.replace("l", "p"))
+print(x.split())
+print(y.split())
+# boolenans
+print(20 > 10)
+print(67 < 87)
+print(90 > 78)
+print(67 < 78)
+print(90 < 96)
+print(90 == 56)
+print(bool('hello'))
+print(bool(999))
+# mylistcjgiuv
+mylist = [10, 40, 758, "eren yeger", 'captian levi', "bikasu"]
+print(mylist[4])
+print(mylist[3])
+print(mylist[-2])
+print(mylist[-1])
+print(mylist[4])
+print(mylist[2])
+print(mylist[2+1])
+print(mylist[0+2])
+print(mylist[0+5])
+print(mylist)
+mylist[1:4] = [200, 'mikassa']
+print(mylist)
+mylist[1:4] = [67, 789, 987, 'vecna', 'nezukoo', 'okyaar']
+print(mylist)
+mylist = [10, 40, 758, "eren yeger", 'captian levi', "bikasu"]
+mylist1 = [56, 777, 90, "galadriel", 'sauron', "durin"]
+mylist1.extend(mylist)
+print(mylist)
+print(mylist1)
+# yhj
+mylist = ["apples", "eren", "koijkl"]
+print(mylist)
+mylist.clear()
+print(mylist)
+# python operators
+# arethemtic opertor
+print(10+20)
+print(20-12)
+print(30*5)
+print(30**5)
+print(40/5)
+print(40//2)
+print(56 % 2)
+# assignment operator
+a = 27
+print(a)
+a += 3
+print(a)
+a -= 8
+print(a)
+a = a*5
+print(a)
+a = a/9
+print(a)
+a = a//9
+print(a)
+a = a % 3
+print(a)
+# ccamparison
+print(67 == 768)
+print(67 != 768)
+print(67 <= 768)
+print(67 >= 768)
+print(6798 < 768)
+print(67 > 768)
